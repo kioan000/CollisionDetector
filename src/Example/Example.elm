6b3c9ec4a44d0442d963e824467a6fb60773a513
Ivan Gori <ivan.gori@prima.it>
2021-01-26 11:50:13 +0100

    wip: remove old versions

--- src/Example/Example.elm
+++ src/Example/Example.elm
@@ -2,92 +2,36 @@ module Example.Example exposing (main)
 
 import Browser exposing (Document)
 import Browser.Dom as Dom
-import Collidable exposing (Collidable)
-import CollisionDetector exposing (CollisionDetector)
+import Collidable as Collidable exposing (Collidable)
+import CollisionDetector as CollisionDetector exposing (CollisionDetector)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (on)
 import Json.Decode as Json
-import Process
-import Task exposing (Task)
 import Utils.Update as UtilsUpdate
 
 
 type Msg
-    = IterateCollisionDetection
-    | AddCollidable CollidableType Float Float Float Float
-    | UpdateCollidablePosition CollidableType Float Float
-    | Error ErrorType String
-
-
-type ErrorType
-    = ElementNotFound
-
-
-type CollidableType
-    = Square1
-    | Square2
-    | Path1
-    | Path2
-    | Path3
-    | Path4
-    | Path5
-
-
-collidableTypeToId : CollidableType -> String
-collidableTypeToId collidableType =
-    case collidableType of
-        Square1 ->
-            "square1"
-
-        Square2 ->
-            "square2"
-
-        Path1 ->
-            "path1"
-
-        Path2 ->
-            "path2"
-
-        Path3 ->
-            "path3"
-
-        Path4 ->
-            "path4"
-
-        Path5 ->
-            "path5"
+    = SquareCollision (Collidable Msg) (Collidable Msg)
+    | CollisionDetectorMsg (CollisionDetector.Msg Msg)
+    | CollisionDetectorError Dom.Error
 
 
 type alias Model =
-    { collisionDetector : CollisionDetector CollisionDetectorScope Msg
-    , collisionDetectorScope : CollisionDetectorScope
-    }
-
-
-type alias CollisionDetectorScope =
-    { square2Attributes : List (Html.Attribute Msg)
-    , path1Attributes : List (Html.Attribute Msg)
-    , path2Attributes : List (Html.Attribute Msg)
-    , path3Attributes : List (Html.Attribute Msg)
-    , path4Attributes : List (Html.Attribute Msg)
-    , path5Attributes : List (Html.Attribute Msg)
+    { collisionDetector : CollisionDetector Msg
     , message : String
+    , attrs : List (Html.Attribute Msg)
     }
 
 
 initialModel : Model
 initialModel =
-    { collisionDetector = CollisionDetector.create
-    , collisionDetectorScope =
-        { square2Attributes = []
-        , path1Attributes = []
-        , path2Attributes = []
-        , path3Attributes = []
-        , path4Attributes = []
-        , path5Attributes = []
-        , message = ""
-        }
+    { collisionDetector =
+        CollisionDetector.collisionDetector
+            CollisionDetectorMsg
+            CollisionDetectorError
+    , message = ""
+    , attrs = []
     }
 
 
@@ -95,16 +39,16 @@ view : Model -> Document Msg
 view model =
     { title = "Example"
     , body =
-        [ div [ style "height" "100vh", style "overflow" "scroll", style "position" "relative", onScroll IterateCollisionDetection ]
+        [ div [ style "height" "100vh", style "overflow" "scroll", style "position" "relative", onScroll (CollisionDetector.tickEvent model.collisionDetector) ]
             [ div [ style "display" "flex", style "flex-flow" "column", style "min-height" "300vh" ]
-                [ h1 [ style "position" "fixed", style "left" "0", style "top" "0" ] [ text model.collisionDetectorScope.message ]
+                [ h1 [ style "position" "fixed", style "left" "0", style "top" "0" ] [ text model.message ]
                 , square1
-                , path (collidableTypeToId Path1) model.collisionDetectorScope.path1Attributes
-                , path (collidableTypeToId Path2) model.collisionDetectorScope.path2Attributes
-                , box2 model.collisionDetectorScope.square2Attributes
-                , path (collidableTypeToId Path3) model.collisionDetectorScope.path3Attributes
-                , path (collidableTypeToId Path4) model.collisionDetectorScope.path4Attributes
-                , path (collidableTypeToId Path5) model.collisionDetectorScope.path5Attributes
+                , path "path1" []
+                , path "path2" []
+                , box2 model.attrs
+                , path "path3" []
+                , path "path4" []
+                , path "path5" []
                 ]
             ]
         ]
@@ -119,7 +63,7 @@ onScroll msg =
 square1 : Html Msg
 square1 =
     div
-        [ id (collidableTypeToId Square1)
+        [ id "square1"
         , style "height" "100px"
         , style "width" "100px"
         , style "position" "fixed"
@@ -136,7 +80,7 @@ box2 : List (Html.Attribute Msg) -> Html Msg
 box2 attributes =
     div
         (List.append attributes
-            [ id (collidableTypeToId Square2)
+            [ id "square2"
             , style "height" "100px"
             , style "width" "100px"
             , style "left" "50%"
@@ -161,6 +105,7 @@ path idValue attributes =
         []
 
 
+subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch []
 
@@ -168,168 +113,36 @@ subscriptions model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        IterateCollisionDetection ->
+        SquareCollision square collidable ->
             model
-                |> UtilsUpdate.withCmds
-                    [ Task.attempt (resultToUpdateCollidablePosition Square1) (Dom.getElement (collidableTypeToId Square1))
-                    , Task.attempt (resultToUpdateCollidablePosition Square2) (Dom.getElement (collidableTypeToId Square2))
-                    , Task.attempt (resultToUpdateCollidablePosition Path1) (Dom.getElement (collidableTypeToId Path1))
-                    , Task.attempt (resultToUpdateCollidablePosition Path2) (Dom.getElement (collidableTypeToId Path2))
-                    , Task.attempt (resultToUpdateCollidablePosition Path3) (Dom.getElement (collidableTypeToId Path3))
-                    , Task.attempt (resultToUpdateCollidablePosition Path4) (Dom.getElement (collidableTypeToId Path4))
-                    , Task.attempt (resultToUpdateCollidablePosition Path5) (Dom.getElement (collidableTypeToId Path5))
-                    ]
-                |> CollisionDetector.cycleWithCmds model.collisionDetector
-
-        AddCollidable collidableType x y w h ->
-            model
-                |> addCollidable collidableType x y w h
-                |> UtilsUpdate.withCmds []
-
-        UpdateCollidablePosition collidableType x y ->
-            model
-                |> updateCollisionDetector (CollisionDetector.updatePosition (collidableTypeToId collidableType) x y)
+                |> setMessage (Collidable.pickId square ++ " collided with " ++ Collidable.pickId collidable)
                 |> UtilsUpdate.withoutCmds
 
-        Error ElementNotFound id ->
+        CollisionDetectorMsg cdMsg ->
             model
-                |> UtilsUpdate.withoutCmds
+                |> CollisionDetector.update cdMsg
+
+        CollisionDetectorError error ->
+            model |> UtilsUpdate.withoutCmds
 
 
-addCollidable : CollidableType -> Float -> Float -> Float -> Float -> Model -> Model
-addCollidable collidableType x y w h model =
-    { model | collisionDetector = CollisionDetector.addCollidable (collidable collidableType x y w h) model.collisionDetector }
-
-
-collidable : CollidableType -> Float -> Float -> Float -> Float -> Collidable CollisionDetectorScope Msg
-collidable collidableType x y w h =
-    case collidableType of
-        Square1 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision square1Collision
-
-        Square2 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision square2Collision
-
-        Path1 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision path1Collision
-
-        Path2 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision path2Collision
-
-        Path3 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision path3Collision
-
-        Path4 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision path4Collision
-
-        Path5 ->
-            Collidable.create (collidableTypeToId collidableType) x y w h
-                |> Collidable.onCollision path5Collision
-
-
-square1Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-square1Collision me collided scope =
-    scope
-        |> UtilsUpdate.withoutCmds
-
-
-square2Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-square2Collision me collided scope =
-    { scope
-        | square2Attributes = [ style "border-radius" "50px" ]
-        , message = "square collided"
-    }
-        |> UtilsUpdate.withoutCmds
-
-
-path1Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-path1Collision me collided scope =
-    { scope
-        | path1Attributes = [ style "opacity" "20%" ]
-        , message = "path1 collided"
-    }
-        |> UtilsUpdate.withoutCmds
-
-
-path2Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-path2Collision me collided scope =
-    { scope
-        | path2Attributes = [ style "opacity" "20%" ]
-        , message = "path2 collided"
-    }
-        |> UtilsUpdate.withoutCmds
-
-
-path3Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-path3Collision me collided scope =
-    { scope
-        | path3Attributes = [ style "opacity" "20%" ]
-        , message = "path3 collided"
-    }
-        |> UtilsUpdate.withoutCmds
-
-
-path4Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-path4Collision me collided scope =
-    { scope
-        | path4Attributes = [ style "opacity" "20%" ]
-        , message = "path4 collided"
-    }
-        |> UtilsUpdate.withoutCmds
-
-
-path5Collision : Collidable CollisionDetectorScope Msg -> Collidable CollisionDetectorScope Msg -> CollisionDetectorScope -> ( CollisionDetectorScope, Cmd msg )
-path5Collision me collided scope =
-    { scope
-        | path5Attributes = [ style "opacity" "20%" ]
-        , message = "path5 collided"
-    }
-        |> UtilsUpdate.withoutCmds
-
-
-updateCollisionDetector : (CollisionDetector CollisionDetectorScope Msg -> CollisionDetector CollisionDetectorScope Msg) -> Model -> Model
-updateCollisionDetector mapper model =
-    { model | collisionDetector = mapper model.collisionDetector }
+setMessage : String -> Model -> Model
+setMessage msg model =
+    { model | message = msg }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
     initialModel
-        |> UtilsUpdate.withCmds
-            [ Task.attempt (resultToAddCollidable Square1) (Dom.getElement (collidableTypeToId Square1))
-            , Task.attempt (resultToAddCollidable Square2) (Dom.getElement (collidableTypeToId Square2))
-            , Task.attempt (resultToAddCollidable Path1) (Dom.getElement (collidableTypeToId Path1))
-            , Task.attempt (resultToAddCollidable Path2) (Dom.getElement (collidableTypeToId Path2))
-            , Task.attempt (resultToAddCollidable Path3) (Dom.getElement (collidableTypeToId Path3))
-            , Task.attempt (resultToAddCollidable Path4) (Dom.getElement (collidableTypeToId Path4))
-            , Task.attempt (resultToAddCollidable Path5) (Dom.getElement (collidableTypeToId Path5))
+        |> UtilsUpdate.withCmdsMap
+            [ .collisionDetector >> CollisionDetector.addCollidable "square1" (Just SquareCollision)
+            , .collisionDetector >> CollisionDetector.addCollidable "path1" Nothing
+            , .collisionDetector >> CollisionDetector.addCollidable "path2" Nothing
+            , .collisionDetector >> CollisionDetector.addCollidable "square2" Nothing
+            , .collisionDetector >> CollisionDetector.addCollidable "path3" Nothing
+            , .collisionDetector >> CollisionDetector.addCollidable "path4" Nothing
+            , .collisionDetector >> CollisionDetector.addCollidable "path5" Nothing
             ]
-
-
-resultToAddCollidable : CollidableType -> Result Dom.Error Dom.Element -> Msg
-resultToAddCollidable collidableType result =
-    case result of
-        Ok value ->
-            AddCollidable collidableType value.element.x value.element.y value.element.width value.element.height
-
-        Err (Dom.NotFound id) ->
-            Error ElementNotFound id
-
-
-resultToUpdateCollidablePosition : CollidableType -> Result Dom.Error Dom.Element -> Msg
-resultToUpdateCollidablePosition collidableType result =
-    case result of
-        Ok value ->
-            UpdateCollidablePosition collidableType value.element.x value.element.y
-
-        Err (Dom.NotFound id) ->
-            Error ElementNotFound id
 
 
 main : Program () Model Msg
@@ -340,10 +153,3 @@ main =
         , update = update
         , subscriptions = subscriptions
         }
-
-
-loop : Float -> Task error value -> Task error ()
-loop everyMillis task =
-    task
-        |> Task.andThen (\_ -> Process.sleep everyMillis)
-        |> Task.andThen (\_ -> loop everyMillis task)
